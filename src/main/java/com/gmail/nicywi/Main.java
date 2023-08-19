0f7abf4827a3a8bafef6d5c76c1187daf76d2e68
Gabi <gabrysia.czajkowska@wp.pl>
2023-08-19 12:05:52 +0200

    initial

--- src/main/java/com/gmail/nicywi/Main.java
+++ src/main/java/com/gmail/nicywi/Main.java
@@ -1,7 +1,14 @@
 package com.gmail.nicywi;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user1 = new User ("John", "john@gmail.com");
+        User user2 = new User ("Anna", "anna@gmail.com");
+
+        Mail mail1 = new Mail ("Wedding invitation", "Hi Anna blabla",LocalDate.now());
+        Mail mail2 = new Mail ("Wedding reply", "Hi John blabla",LocalDate.now());
+
     }
 }
