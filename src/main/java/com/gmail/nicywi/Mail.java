4ddcf5de17339069a8368c2cafd0cfb2a86f20c4
nikola <nicywi@gmail.com>
2023-08-19 11:38:11 +0200

    User and Mail

---
+++ src/main/java/com/gmail/nicywi/Mail.java
@@ -0,0 +1,39 @@
+package com.gmail.nicywi;
+
+import java.time.LocalDate;
+
+public class Mail {
+    private String title;
+    private String description;
+    private LocalDate date;
+
+    public Mail(String title, String description, LocalDate date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
