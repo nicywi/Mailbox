2fb553c326d2ee95042edbd7d34b69d23a642443
nikola <nicywi@gmail.com>
2023-08-19 15:59:13 +0200

    Main updates x 2

--- src/main/java/com/gmail/nicywi/MailService.java
+++ src/main/java/com/gmail/nicywi/MailService.java
@@ -12,10 +12,27 @@ public class MailService {
         mails.add(mail);
     }
     public List<Mail> getReceivedEmails (User user){
-        for (:
-             ) {
-            
+
+        List<Mail> receivedEmails =null;
+
+        for (Mail mail: mails) {
+            if(mail.getReceiver().getLogin().equals(user.getLogin())){
+                receivedEmails.add(mail);
+            }
         }
+        return receivedEmails;
+    }
+
+    public List<Mail> getSentEmails (User user){
+
+        List<Mail> sentEmails =null;
+
+        for (Mail mail: mails) {
+            if(mail.getSender().getLogin().equals(user.getLogin())){
+                sentEmails.add(mail);
+            }
+        }
+        return sentEmails;
     }
 
 
