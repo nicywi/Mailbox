02d782e10e6551107b73abcfdc51c80338c8897f
nikola <nicywi@gmail.com>
2023-08-20 10:23:09 +0200

    GetReceiverEmails fix

--- src/main/java/com/gmail/nicywi/MailService.java
+++ src/main/java/com/gmail/nicywi/MailService.java
@@ -4,38 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MailService {
-
     private final List<Inbox> inboxes = new ArrayList<>();
     private final List<Mail> mails = new ArrayList<Mail>();
 
     public void send(Mail mail) {
         mails.add(mail);
     }
-    public List<Mail> getReceivedEmails (User user){
 
+    public List<Mail> getReceivedEmails(User user) {
         List<Mail> receivedEmails = new ArrayList<>();
 
-        for (Mail mail: mails) {
-            if(mail.getReceiver().getLogin().equals(user.getLogin())){
-                receivedEmails.add(mail);
-            }
-        }
+        mails.stream().filter(mail -> mail.getReceiver().getLogin().equals(user.getLogin()))
+                .forEach(receivedEmails::add);
+
         return receivedEmails;
     }
 
-    public List<Mail> getSentEmails (User user){
-
+    public List<Mail> getSentEmails(User user) {
         List<Mail> sentEmails = new ArrayList<>();
 
-        for (Mail mail: mails) {
-            if(mail.getSender().getLogin().equals(user.getLogin())){
-                sentEmails.add(mail);
-            }
-        }
+        mails.stream().filter(mail -> mail.getSender().getLogin().equals(user.getLogin()))
+                .forEach(sentEmails::add);
         return sentEmails;
     }
-
-
 
     public List<Inbox> getInboxes() {
         return inboxes;
